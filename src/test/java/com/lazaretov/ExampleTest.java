828514cbaf24d9d8a41b1c49258dca51bcacd6f8
Timofei <lazaretov03@gmail.com>
2022-09-15 00:12:44 +0300

    Init commit from console

---
+++ src/test/java/com/lazaretov/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.lazaretov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue(2<3);
+    }
+
+}
