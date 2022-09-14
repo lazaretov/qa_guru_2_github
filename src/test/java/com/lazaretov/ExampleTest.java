c2990b731d5052abc5ffd94b2735401b82b74d5d
Timofei <lazaretov03@gmail.com>
2022-09-15 00:57:59 +0300

    use random int

--- src/test/java/com/lazaretov/ExampleTest.java
+++ src/test/java/com/lazaretov/ExampleTest.java
@@ -3,15 +3,17 @@ package com.lazaretov;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2<3);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(4>3);
+        Assertions.assertTrue(new Random().nextInt() > 2);
     }
 }
