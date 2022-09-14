b41bf5c7d4ec166c797654dcb6a19d1c9624ddac
Timofei <lazaretov03@gmail.com>
2022-09-15 00:39:43 +0300

    added second test

--- src/test/java/com/lazaretov/ExampleTest.java
+++ src/test/java/com/lazaretov/ExampleTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test;
 public class ExampleTest {
 
     @Test
-    void exampleTest() {
+    void exampleTest0() {
         Assertions.assertTrue(2<3);
     }
 
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(4>3);
+    }
 }
